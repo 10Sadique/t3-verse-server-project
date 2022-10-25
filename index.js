d9c36d9a83548bf9e85353260203820a32949c6b
10Sadique <jafarsjahan@gmail.com>
2022-10-25 17:14:41 +0600

    single course api end point added

--- index.js
+++ index.js
@@ -16,6 +16,14 @@ app.get('/courses', (req, res) => {
     res.send(courses);
 });
 
+app.get('/courses/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+
+    const selectedCourse = courses.find((course) => course.id === id);
+
+    res.send(selectedCourse);
+});
+
 app.listen(port, () => {
     console.log('Listening to port: ', port);
 });
