2b77f6564398308f349b4acb74fa1373e75a356a
10Sadique <jafarsjahan@gmail.com>
2022-10-25 17:10:21 +0600

    all courses api endpoint added

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const express = require('express');
 const cors = require('cors');
 const port = 5000;
 
+const courses = require('./data/courses.json');
+
 const app = express();
 
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send({ message: 't3-verse Server.' });
+});
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
 });
 
 app.listen(port, () => {
